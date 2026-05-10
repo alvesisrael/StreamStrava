fa0bf858ceccad1827faf221e822bdafea194a17
Israel Martins <israel.martins@gam.com.br>
2026-05-10 11:05:15 -0300

    claude mapa

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 import plotly.graph_objects as go
+import streamlit.components.v1 as components
 
 try:
     import pydeck as pdk
